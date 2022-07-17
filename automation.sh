deb613f7d5ea86d632b3c5b6a457f85cca452010
Mohanad Abugharbia <mgharbieh.01@gmail.com>
2022-07-17 02:38:46 +0200

    Introduced a setup parameter, which makes sure dependencies are installed in order for the scripts to start

--- automation.sh
+++ automation.sh
@@ -1,9 +1,53 @@
 #!/bin/bash
 
 
+setup(){
+    echo "Starting setup..."
+    program_path=$dir_path/automation.sh
+    program_name=automation
+
+    python3 --version > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo $'\e[1;31m'ERROR:$'\e[0m python3 is not installed.'
+        echo 'Please install python3 before running the setup again.'
+        exit 1
+    fi 
+
+    if [ ! -L "/usr/local/bin/$program_name" ]; then
+        ln -s $program_path /usr/local/bin/$program_name > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo "Successfully created a symlink"
+            echo "You can run the automation scripts by using the command <$program_name> from your terminal!"
+        else
+            echo $'\e[1;31m'ERROR:$'\e[0m Symlink creation failed!'
+            if [ $USER != "root" ]; then
+                echo "Probably due to the setup not being run with root permissions."
+            fi
+            echo "To manually create the symlink use the following command: "
+            echo $'\e[1;33m'Command:$'\e[0m' "sudo ln -s $program_path /usr/local/bin/$program_name"
+        fi
+    fi
+
+    for file in $absolute_requirements_path*
+    do
+        absolute_file_path=$file
+        file=$( echo $file |rev |cut -f 1 -d "/"| rev )
+        case "$( echo $file |cut -f 1 -d "_" )" in
+            python)
+                python3 -m pip install -r $absolute_file_path > /dev/null 2>&1
+                if [ $? -eq 0 ]; then
+                    echo "Successfully installed python requirements!"
+                else
+                    echo $'\e[1;31m'ERROR:$'\e[0m an unexpected error occured while installing python requirements'
+                fi;;
+            *) 
+                echo "Skipping requirements file: '$file', Unkown type!";;
+        esac
+    done
+}
+
 kill_process(){
-    if [ $# -ne 1 ]
-    then
+    if [ $# -ne 1 ]; then
         echo "Please enter the name of the process that should be killed"
         exit 1
     fi
@@ -15,33 +59,44 @@ kill_process(){
 }
 
 start_python_script(){
-    if [ $# -ne 1 ]
-    then 
+    if [ $# -ne 1 ]; then 
         echo "You did not give a script name to the function <start_python_script>"
         exit 1
     fi
     echo "starting $1"
-    python $programs_path$1 &
+    python3 $1 &
 }
 
-main(){
+start(){
     echo "Starting automation scripts!"
-    prg_name=$( echo $0 |rev |cut -f 1 -d "/" |rev |cut -f 1 -d "." )
-    programs_path=$HOME/automation-scripts/
-    for file in $programs_path*
-    do 
-        file=$( echo $file |rev |cut -f 1 -d "/"| rev)
-        if [ $( echo $file |cut -f 1 -d ".") != $prg_name ]
-        then
-            case "$file" in 
-                *.py) 
-                    kill_process $file
-                    start_python_script $file;;
-                *) echo "Skipping '$file', because it has an unknown File type";;
-            esac
-        fi
+    for script in $absolute_scripts_path*; do 
+        absolute_script_path=$script
+        script=$( echo $script |rev |cut -f 1 -d "/"| rev)
+        case "$script" in 
+            *.py) 
+                kill_process $script
+                start_python_script $absolute_script_path;;
+            *) echo "Skipping script: '$script', Unknown script type!";;
+        esac
     done
 }
 
+usage(){
+    echo "USAGE: <$( echo $0 |rev |cut -f 1 -d '/' |rev )> setup|run"
+    exit 1
+}
 
-main
+dir_path=$( readlink -f $0 |rev |cut -f2- -d "/" |rev )
+absolute_scripts_path=$dir_path/scripts/
+absolute_requirements_path=$dir_path/requirements/
+
+
+
+case "$1" in
+    setup)
+        setup;;
+    run)
+        start;;
+    *)
+        usage
+esac
